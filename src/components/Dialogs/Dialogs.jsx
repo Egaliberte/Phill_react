e88c6179dbf34ed8a386ca9761635fa56960d7ac
Egaliberte <valentin.stepanenko.psy@gmail.com>
2022-03-12 16:08:36 +0300

    5 version

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -1,13 +1,43 @@
 import React from 'react';
 import './Dialogs.css';
+import { Link } from 'react-router-dom';
 
+const DialogItem = (props) => {
+    let path = '/dialogs/' + props.id;
+    return (
+        <div className='items'>
+            <Link to={path}> {props.name} </Link>
+
+        </div>
+    )
+}
+const MessageItem = (props) => {
+    return (
+        <div className="message">
+            {props.message}
+
+        </div>
+    )
+}
 
 const Dialogs = () => {
     return (
         <div className='Dialogs'>
-            <h1>Dialogs Tet a Tet </h1>
-            {/* <iframe width="1000" height="600" src="https://www.youtube.com/embed/GBs-vSwOTAE" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe> */}
-            
+            <div className='Dialogs-items'>
+                <DialogItem name="Valentin Stepanenko" id="1" />
+                <DialogItem name="Polina" id="2" />
+                <DialogItem name="Zarina" id="3" />
+                <DialogItem name="Ksenia" id="4" />
+                <DialogItem name="Egor" id="5" />
+
+
+            </div>
+            <div className='Messages'>
+                <MessageItem message ="Hello" />
+                <MessageItem message ="Hi" />
+                <MessageItem message ="Fuck" />
+            </div>
+
         </div>
     )
 }
